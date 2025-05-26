20648bc219417e1b27226e355ea3d72df5f3a575
didin <didin1453fatih@gmail.com>
2025-05-26 10:13:48 +0700

    add analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
-import Image from 'next/image'
-
+import Image from "next/image";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   display: "swap",
@@ -31,8 +31,20 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b h-16">
                 <div className="w-full max-w-7xl flex justify-between items-center sm:px-7 px-3 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                  <Image src={`/engineer_help_engineer_text_only.png`} alt="logo"  height="54" width="190" className="block dark:hidden" />
-                  <Image src={`/engineer_help_engineer_text_only_white.png`} alt="logo"  height="54" width="190" className="hidden dark:block" />
+                    <Image
+                      src={`/engineer_help_engineer_text_only.png`}
+                      alt="logo"
+                      height="54"
+                      width="190"
+                      className="block dark:hidden"
+                    />
+                    <Image
+                      src={`/engineer_help_engineer_text_only_white.png`}
+                      alt="logo"
+                      height="54"
+                      width="190"
+                      className="hidden dark:block"
+                    />
                   </div>
                   <HeaderAuth />
                 </div>
@@ -42,19 +54,14 @@ export default function RootLayout({
               </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs  py-2">
-                <p>
-                  Build with ❤️ by Dienastya Galih
-                </p>
-                <p className="px-3">
-                  |
-                </p>
-                <p>
-                  Theme
-                </p>
+                <p>Build with ❤️ by Dienastya Galih</p>
+                <p className="px-3">|</p>
+                <p>Theme</p>
                 <ThemeSwitcher />
               </footer>
             </div>
           </main>
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
